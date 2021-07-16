130ddd15fc69dc9473ef0aa2c5d4c8be07ef5526
Bruno Bezerra Bastos <brn.suncity@gmail.com>
2021-07-16 09:57:40 -0300

    Correção de erros menores

--- SLAM localização.py
+++ SLAM localização.py
@@ -33,7 +33,7 @@ def tcomp(tab, tbc):
 def tinv(tab):
 	tba = np.zeros(tab.size)
 	for t in range(0, tab[0].size, 3):
-		tba = tinv1(tab[t:t+3]) # POSSÍVEL FONTE DE PROBLEMAS, CHECAR O CÓDIGO ORIGINAL
+		tba = tinv1(tab[t:t+3])
 	return tba
 
 def tinv1(tab):
@@ -45,15 +45,17 @@ def tinv1(tab):
 ##################################### FUNÇÕES
 def GetRobotControl(k):
 	return np.array([0, 0.025, 0.1 * np.pi / 180 * np.sin(3 * np.pi * k / nSteps)]).reshape(3, 1)
+	# return np.array([0, 0.025, 0]).reshape(3, 1)
 
 def GetOdometry(k):
 	# Aparentemente python não tem uma forma persistente como 'static' do C++ ou 'persistent' do MATLAB, mas isso deve funcionar:
 	# GetOdometry.LastOdom # internal to Robot low-level controller
+	global UTrue
 	if k == 0:
 		GetOdometry.LastOdom = np.copy(XTrue)
 	u = GetRobotControl(k)
 	xnow = tcomp(GetOdometry.LastOdom, u)
-	uNoise = np.sqrt(UTrue).dot(random.rand(3, 1))
+	uNoise = np.sqrt(UTrue).dot(random.normal(0, 1, (3, 1)))
 	xnow = tcomp(xnow, uNoise)
 	GetOdometry.LastOdom = np.copy(xnow)
 	return xnow
@@ -82,14 +84,13 @@ def doObservationModel(xVeh, iFeature, Map):
 	z[1] = angleWrap(z[1])
 	return z
 
-
 def J1(x1, x2):
 	s1 = np.sin(x1[2])
 	c1 = np.cos(x1[2])
 
 	Jac = np.array([[1, 0, -x2[0] * s1 - x2[1] * c1],
 			   [0, 1, x2[0] * c1 - x2[1] * s1],
-			   [0, 0, 1]])
+			   [0, 0, 1]], dtype = object)
 	return Jac
 
 def J2(x1, x2):
@@ -98,7 +99,7 @@ def J2(x1, x2):
 
 	Jac = np.array([[c1, -s1, 0],
 			   [s1, c1, 0],
-			   [0, 0, 1]])
+			   [0, 0, 1]], dtype = object)
 	return Jac
 
 def GetObservation(k):
@@ -109,26 +110,65 @@ def GetObservation(k):
 		iFeature = -1
 	else:
 		iFeature = int(np.floor(Map[0].size * np.random.uniform()))
-		z = doObservationModel(XTrue, iFeature, Map)# + np.sqrt(np.diag(RTrue)).reshape(2, 1) * random.normal(0, 1, size = (2, 1))
+		z = doObservationModel(XTrue, iFeature, Map) + np.sqrt(np.diag(RTrue)).reshape(2, 1) * random.normal(0, 1, size = (2, 1))
 		z[1] = angleWrap(z[1])
 	return z, iFeature
 
 def DoGraphs():
+	global XTrue, XEst, PEst
+	plt.clf()
 	plt.axis([-80, 80, -80, 80])
 	plt.title('Localisation')
 	plt.scatter(Map[0, :], Map[1, :], color = 'green')
-	DrawRobot(x)
+	PlotEllipse(XEst, PEst, 3)
+	DrawRobot(XEst, 'red')
+	DrawRobot(XTrue, 'blue')
+	DrawRobot(XPredOnly, 'green')
 	plt.pause(0.001)
+
+def DrawRobot(Xr, col):
+	p = 0.02
+	xmin, xmax, ymin, ymax = plt.axis()
+	l1 = (xmax - xmin) * p
+	l2 = (ymax - ymin) * p
+	P = np.array([[-1, 1, 0, -1], [-1, -1, 3, -1]])
+	theta = Xr[2]
+	c = math.cos(theta)
+	s = math.sin(theta)
+	rotation = np.array([[c, -s], [s, c]], dtype = float)
+	P = rotation.dot(P)
+	P[0, :] = P[0, :] * l1 + Xr[0]
+	P[1, :] = P[1, :] * l2 + Xr[1]
+	plt.plot(P[0, :], P[1, :], color = col)
+	plt.plot(Xr[0], Xr[1], color = col)
+
+def PlotEllipse(x, Po, nSigma):
+	x = np.copy(x[0:2])
+	P = np.empty((2, 2)) # Copiando dessa forma por causa dos problemas de formatação da np
+	for i in range(2):
+		for j in range(2):
+			P[i, j] = Po[i, j][0]
+	if not 0 in np.diag(P):
+		D, V = np.linalg.eig(P)
+		D = np.diag(D)
+		s = np.linspace(0, 2 * np.pi, 50)
+		pX = []
+		pY = []
+		for i in s:
+			y = nSigma * np.array([[np.cos(i)], [np.sin(i)]])
+			el = V.dot(LA.sqrtm(D)).dot(y)
+			el =  np.matlib.repmat(x, 1, len(el[0]) + 1) + np.concatenate((el, el), axis = 1)
+			pX += [el[0,0]]
+			pY += [el[1,0]]
+		plt.plot(pX, pY, color = 'red')
 
 ###################################################################### EXECUÇÃO
 nSteps = 6000
 
 Map = 140 * np.random.uniform(size = (2, 10)) - 70
 
-# UTrue = np.diag([0.01, 0.01, 1 * np.pi / 180])**2
-UTrue = np.diag([0, 0, 0])**2
-# RTrue = np.diag([2, 3 * np.pi / 180])**2
-RTrue = np.diag([0, 0])**2
+UTrue = np.diag([0.01, 0.01, 1 * np.pi / 180])**2
+RTrue = np.diag([2, 3 * np.pi / 180])**2
 
 UEst = 1 * UTrue
 REst = 1 * RTrue
@@ -138,20 +178,21 @@ XTrue = np.array([1, -40, -np.pi / 2]).reshape(3, 1)
 xOdomLast = GetOdometry(0)
 
 XEst = np.copy(XTrue)
+XPredOnly = np.copy(XTrue)
 PEst = np.diag([1, 1, (1 * np.pi / 180)**2])
 
-
-for k in range(1, nSteps):
-	plt.clf()
+gif = 0
+for k in range(1, nSteps+1):
 
 	simulateWorld(k)
 	xOdomNow = GetOdometry(k)
 	u = tcomp(tinv(xOdomLast), xOdomNow)
 	xOdomLast = np.copy(xOdomNow)
 
-	print(k)
 	XPred = tcomp(XEst, u) 
 	XPred[2] = angleWrap(XPred[2])
+	# XPredOnly = np.copy(XPred)
+	XPredOnly = tcomp(XPredOnly, u)
 	PPred = J1(XEst, u).dot(PEst).dot(J1(XEst, u).T) + J2(XEst, u).dot(UEst).dot(J2(XEst, u).T)
 	z, iFeature = GetObservation(k)
 
@@ -181,10 +222,9 @@ for k in range(1, nSteps):
 		innov = np.empty((2, 1))
 		# S = np.eye(2) * None
 
-
-	# DoGraphs()
-	
-
-	
+	if k%15 == 1:
+		DoGraphs()
+		# plt.savefig('gif' + str(gif) + '.jpg')
+		# gif+=1
 
 plt.show()
