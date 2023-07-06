2f4e5dc2d9cf48d163df080e06c9158546c77095
BrunoBBastos <brn.suncity@gmail.com>
2023-07-06 00:30:31 -0300

    Modularizando a interface

--- Servidor SLAM/InterfaceSLAM.py
+++ Servidor SLAM/InterfaceSLAM.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageTk
 import threading
 import json
 
+
 class InterfaceModule:
     def __init__(self, ip, video_url, page_url, interval_ms):
         self.ip = f'http://{ip}'
@@ -15,46 +16,46 @@ class InterfaceModule:
         self.root = tk.Tk()
         self.root.title("Robot Interface")
 
-        self.pose_frame = tk.LabelFrame(self.root, text="Pose", padx=10, pady=10)
-        self.pose_frame.pack(side="left", padx=10, pady=10)
-
-        self.referencia_frame = tk.LabelFrame(self.root, text="Referencia", padx=10, pady=10)
-        self.referencia_frame.pack(side="left", padx=10, pady=10)
-
-        self.x_label = tk.Label(self.pose_frame, text="x:")
-        self.x_label.grid(row=0, column=0, sticky="e", padx=5, pady=5)
-
-        self.y_label = tk.Label(self.pose_frame, text="y:")
-        self.y_label.grid(row=1, column=0, sticky="e", padx=5, pady=5)
-
-        self.theta_label = tk.Label(self.pose_frame, text="theta:")
-        self.theta_label.grid(row=2, column=0, sticky="e", padx=5, pady=5)
-
-        self.referencia_x_label = tk.Label(self.referencia_frame, text="x:")
-        self.referencia_x_label.grid(row=0, column=0, sticky="e", padx=5, pady=5)
-
-        self.referencia_y_label = tk.Label(self.referencia_frame, text="y:")
-        self.referencia_y_label.grid(row=1, column=0, sticky="e", padx=5, pady=5)
-
-        self.referencia_theta_label = tk.Label(self.referencia_frame, text="theta:")
-        self.referencia_theta_label.grid(row=2, column=0, sticky="e", padx=5, pady=5)
-
-        self.black_screen = Image.new("RGB", (640, 480), "black")
-        self.black_screen_photo = ImageTk.PhotoImage(self.black_screen)
-
-        self.video_label = tk.Label(self.root, image=self.black_screen_photo)
-        self.video_label.pack(side="right", padx=10, pady=10)
+        self.create_pose_frame()
+        self.create_referencia_frame()
+        self.create_video_frame()
 
         self.start_video_stream()
         self.start_page_content_fetching()
 
+    def create_pose_frame(self):
+        self.pose_frame = tk.LabelFrame(
+            self.root, text="Pose", padx=10, pady=10
+        )
+        self.pose_frame.pack(side="left", padx=10, pady=10)
 
+        self.x_label = self.create_label(self.pose_frame, "x:")
+        self.y_label = self.create_label(self.pose_frame, "y:")
+        self.theta_label = self.create_label(self.pose_frame, "\u03B8:")
 
-    def grid_columnconfigure(self, *args, **kwargs):
-        self.root.grid_columnconfigure(*args, **kwargs)
+    def create_referencia_frame(self):
+        self.referencia_frame = tk.LabelFrame(
+            self.root, text="Referencia", padx=10, pady=10
+        )
+        self.referencia_frame.pack(side="left", padx=10, pady=10)
 
-    def grid_rowconfigure(self, *args, **kwargs):
-        self.root.grid_rowconfigure(*args, **kwargs)
+        self.referencia_x_label = self.create_label(self.referencia_frame, "x:")
+        self.referencia_y_label = self.create_label(self.referencia_frame, "y:")
+        self.referencia_theta_label = self.create_label(
+            self.referencia_frame, "\u03B8:"
+        )
+
+    def create_video_frame(self):
+        self.video_frame = tk.Frame(self.root)
+        self.video_frame.pack(side="right", padx=10, pady=10)
+
+        self.video_label = tk.Label(self.video_frame)
+        self.video_label.pack()
+
+    def create_label(self, parent, text):
+        label = tk.Label(parent, text=text)
+        label.pack(side="top", padx=5, pady=5)
+        return label
 
     def start_video_stream(self):
         threading.Thread(target=self.video_stream_worker, daemon=True).start()
@@ -99,21 +100,23 @@ class InterfaceModule:
 
     def update_page_content(self, content):
         try:
+            # Parse the content as JSON
             data = json.loads(content)
             pose_values = data.get('Pose')
             referencia_values = data.get('Referencia')
 
+            # Update pose values
             if pose_values:
                 pose_values = pose_values.split(', ')
                 if len(pose_values) == 3:
                     x, y, theta = map(float, pose_values)
-
                     self.x_label.configure(text=f"x: {x:.3f}")
                     self.y_label.configure(text=f"y: {y:.3f}")
                     self.theta_label.configure(text=f"\u03B8: {theta:.3f}")
                 else:
                     raise ValueError("Invalid content format or missing values")
 
+            # Update referencia values
             if referencia_values:
                 referencia_values = referencia_values.split(', ')
                 if len(referencia_values) == 3:
@@ -128,4 +131,5 @@ class InterfaceModule:
             print(f"Error updating page content: {e}")
 
     def start(self):
+        # Start the Tkinter event loop
         self.root.mainloop()
