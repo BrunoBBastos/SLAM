8c2715ff2c0078f87ec29d7256ff0a7c0c753cbb
Bruno Bezerra Bastos <50341304+BrunoBBastos@users.noreply.github.com>
2023-08-24 19:09:18 -0300

    teste myconfig

--- esp32-cam-webserver/myconfig.h
+++ esp32-cam-webserver/myconfig.h
@@ -2,6 +2,7 @@
  *  Rename this example to 'myconfig.h' and fill in your details.
  *
  *  The local config is in the '.gitignore' file, which helps to keep details secret.
+ teste
  */
 
 /* Give the camera a name for the web interface */
